4aaa55f085caa5ee41de19a67e29f02442b51a25
NAYABPARI <98818099+NAYABPARI@users.noreply.github.com>
2022-09-06 20:06:54 +0500

    Add files via upload

---
+++ mzr1.py
@@ -0,0 +1,42 @@
+#coding=utf-8
+#Writer : somi
+#File cloning and old Account cloning Tool
+########################################################################
+## import lib
+import os,sys
+try: import requests
+except ModuleNotFoundError:print("Js Install Module requests");os.system("python -m pip install requests &> /dev/null")
+try: import bs4
+except ModuleNotFoundError:print("Js Install Module bs4");os.system("python -m pip install bs4 &> /dev/null")
+try: import mechanize
+except ModuleNotFoundError:print("Js Install Module mechanize");os.system("python -m pip install mechanize &> /dev/null")
+try: import gTTS
+except ModuleNotFoundError: os.system("python -m pip install gTTS &> /dev/null")
+## import in
+import requests as req
+import requests as re
+import time,random,json
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,uuid,ipaddress,calendar,requests,mechanize,bs4,sys,os,subprocess,uuid,requests,sys,random,time,re,base64,json,platform
+from datetime import date
+from datetime import datetime
+from time import sleep
+from time import sleep as waktu
+from random import random as acak
+from random import choice as pilih
+from random import randint
+from bs4 import BeautifulSoup
+import requests as ress
+from sys import exit as exit
+class jalan:
+	def __init__(self, z):
+		for e in z + "\n":
+			sys.stdout.write(e)
+			sys.stdout.flush()
+			time.sleep(0.009)
+## import crack
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor as zthreads
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+from concurrent.futures import ThreadPoolExecutor as kikygtg
+from requests.exceptions import ConnectionError
